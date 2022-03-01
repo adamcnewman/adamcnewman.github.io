e5d4b8c56c12592c4e796c022d9947bd7fc5c07e
Adam Newman <77556178+adamcnewman@users.noreply.github.com>
2022-02-28 21:31:56 -0800

    Create clear-contact-form.js

---
+++ assets/js/clear-contact-form.js
@@ -0,0 +1,6 @@
+// https://help.formspree.io/hc/en-us/articles/1500009404742-How-to-clear-a-form-after-submission
+window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
