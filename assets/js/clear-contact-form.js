64365f248efeb41aa455a47eb4f932cdc1fccad1
Adam Newman <77556178+adamcnewman@users.noreply.github.com>
2022-02-28 22:00:34 -0800

    Update clear-contact-form.js

--- assets/js/clear-contact-form.js
+++ assets/js/clear-contact-form.js
@@ -1,8 +1,6 @@
 // https://help.formspree.io/hc/en-us/articles/1500009404742-How-to-clear-a-form-after-submission
-(function () {
-  window.onbeforeunload = () => {
-    for(const form of document.getElementsByTagName('form')) {
-      form.reset();
-    }
+
+window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
   }
-}) ();
